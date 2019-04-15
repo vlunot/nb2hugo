bcc827adaf70ac2491e8a251d626c6495b1376a4
Vincent Lunot <vlunot@gmail.com>
2019-04-15 21:31:15 +0800

    Change version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(name='nb2hugo',
       python_requires='>=3.6',
-      version='0.1.1',
+      version='0.1.2',
       description='A Jupyter notebook to Hugo markdown converter',
       long_description=readme(),
       long_description_content_type='text/markdown',
